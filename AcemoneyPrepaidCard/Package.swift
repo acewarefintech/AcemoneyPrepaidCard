bd66f62403702c2c6e6dcf27668f4f1cf83095ec
Soorej <ios@acewaretechnology.com>
2022-12-15 17:36:59 +0530

    Included path for target in package.swift

--- AcemoneyPrepaidCard/Package.swift
+++ AcemoneyPrepaidCard/Package.swift
@@ -20,9 +20,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(
+//            name: "AcemoneyPrepaidCard",
+//            dependencies: ["Alamofire","SwiftyJSON"]),
+//            path: "Sources",
         .target(
             name: "AcemoneyPrepaidCard",
-            dependencies: ["Alamofire","SwiftyJSON"]),
+            dependencies: ["Alamofire","SwiftyJSON"],
+            path: "Sources"),
         .testTarget(
             name: "AcemoneyPrepaidCardTests",
             dependencies: ["AcemoneyPrepaidCard"]),
