7ae4c58e5c0fe22e516370a2fa32044a1a9a0b1c
Soorej <ios@acewaretechnology.com>
2022-12-16 11:20:05 +0530

    Minor update

--- AcemoneyPrepaidCard/Package.swift
+++ AcemoneyPrepaidCard/Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "AcemoneyPrepaidCard",
-    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -23,7 +22,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AcemoneyPrepaidCard",
-            dependencies: ["Alamofire", "SwiftyJSON"],
-            path: "Sources/AcemoneyPrepaidCard"),
+            dependencies: ["Alamofire", "SwiftyJSON"]),
     ]
 )
